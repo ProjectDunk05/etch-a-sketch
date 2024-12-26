74435b1d939341acbe4b96d15b69402e3a45ae9f
Adam Green <green.adam10@sky.com>
2024-12-26 15:45:42 +0000

    add a loop which creates 256 divs in the conatiner div

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+
+
+for (let i = 1; i <= 256; i++) {
+    const square = document.createElement("div");
+
+    square.classList.add("square");
+    container.appendChild(square);
+};
