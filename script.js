27c23de85adb58e101dff28b1ae967eef46cf93f
Adam Green <green.adam10@sky.com>
2024-12-26 22:26:28 +0000

    add function to create new grid, with the user inputed number of squares per row, when the button is clicked

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-// select elements with ID = "container"
+//add click event listener on button to call the new grid function
 
-const container = document.querySelector("#container");
+const btn = document.querySelector("#prompt");
 
-//create 256 new divs in the container div
+btn.addEventListener("click", newGrid);
 
-for (let i = 1; i <= 256; i++) {
-    const square = document.createElement("div");
+function newGrid() {
 
+    //get a user input for the number of squares in each row
     
-    square.classList.add("square");
-    container.appendChild(square);
+    let squaresPerRow = prompt("Enter number of squares in each row");
+
+    //create new divs in the container div
+    
+    let gridSquares = squaresPerRow * squaresPerRow;
+
+    //find the width and height of each square if the container is 1000px wide
+
+    let width = (1000 - (squaresPerRow - 1)) / squaresPerRow;
+
+    const container = document.querySelector("#container");
+
+    for (let i = 1; i <= gridSquares; i++) {
+        const square = document.createElement("div");
+
+        
+        square.classList.add("square");
+        square.style.width = width + "px";
+        square.style.height = width + "px";
+        container.appendChild(square);
+    };
+
+    //select all elements with class = "square"
+
+    const divs = document.querySelectorAll(".square");
+
+    //for each div add a mouse event which changes the background colour when the mouse enters the div
+
+    divs.forEach(div => {
+        div.addEventListener("mouseenter", (event) => {
+            div.style.backgroundColor = "red";
+        })
+    });
 };
-
-//select all elements with class = "square"
-
-const divs = document.querySelectorAll(".square");
-
-//for each div add a mouse event which changes the background colour when the mouse enters the div
-
-divs.forEach(div => {
-    div.addEventListener("mouseenter", (event) => {
-        div.style.backgroundColor = "red";
-    })
-});
