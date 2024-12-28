a815edcd64482ac38ad305e08f17e9964bd44b51
Adam Green <green.adam10@sky.com>
2024-12-28 14:50:26 +0000

    add function to require the squaresPerRow to be between 1 and 100

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function newGrid() {
     
     let squaresPerRow = prompt("Enter number of squares in each row");
 
+    
+
     //find the width and height of each square if the container is 1000px wide
 
     let width = (960 / squaresPerRow);
@@ -18,16 +20,27 @@ function newGrid() {
 
     container.textContent = "";
 
-    for (let i = 0; i < squaresPerRow * squaresPerRow; i++) {
-        const square = document.createElement("div");
+    // Check if input is a valid number and within a reasonable range
+    
+    if (isNaN(squaresPerRow) || squaresPerRow <= 0 || squaresPerRow > 100) {
+        alert("Please enter a valid number between 1 and 100.");
+        return;
+    } else {
 
+        // create a user inputted amount of divs 
         
-        square.classList.add("square");
-        square.style.width = width + "px";
-        square.style.height = width + "px";
-        
-        container.appendChild(square);
+        for (let i = 0; i < squaresPerRow * squaresPerRow; i++) {
+            const square = document.createElement("div");
+
+            
+            square.classList.add("square");
+            square.style.width = width + "px";
+            square.style.height = width + "px";
+            
+            container.appendChild(square);
+        };
     };
+    
 
     //select all elements with class = "square"
 
